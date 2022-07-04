c2d3720c9da71b1b6d5eab6154cc8e8f055a676b
Andy-PoleStar <xiaohui.hu@polestar.com>
2022-07-04 18:56:12 +0800

    init

---
+++ cap.demo/cap.demo/Controllers/ConsumerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DotNetCore.CAP;
+
+namespace cap.demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsumerController : ControllerBase
+    {
+        [NonAction]
+        [CapSubscribe("test.show.time")]
+        public void ReceiveMessage(DateTime time, [FromCap] CapHeader header)
+        {
+            Console.WriteLine("message time is:" + time);
+            Console.WriteLine("message firset header :" + header["my.header.first"]);
+            Console.WriteLine("message second header :" + header["my.header.second"]);
+        }
+    }
+}
---
+++ cap.demo/cap.demo/Controllers/PublishController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DotNetCore.CAP;
+
+namespace cap.demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishController : ControllerBase
+    {
+        [Route("~/send")]
+        [HttpGet]
+        public IActionResult SendMessage([FromServices] ICapPublisher capBus)
+        {
+            var header = new Dictionary<string, string>()
+            {
+                ["my.header.first"] = "first",
+                ["my.header.second"] = "second"
+            };
+
+            capBus.Publish("test.show.time", DateTime.Now, header);
+            //capBus.Publish("test.show.time", DateTime.Now);
+
+            return Ok();
+        }
+    }
+}
